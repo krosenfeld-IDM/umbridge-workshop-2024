71e7e7f20f460b81f65d96778641154f2283ce56
Katherine Rosenfeld <56368533+krosenfeld-IDM@users.noreply.github.com>
2024-11-29 03:32:45 +0000

    add test for panel

---
+++ tests/app.py
@@ -0,0 +1,67 @@
+# https://panel.holoviz.org/how_to/test/pytest.html
+
+import time
+
+import panel as pn
+import param
+
+class App(pn.viewable.Viewer):
+    run = param.Event(doc="Runs for click_delay seconds when clicked")
+    runs = param.Integer(doc="The number of runs")
+    status = param.String(default="No runs yet")
+
+    load_delay = param.Number(default=0.5)
+    run_delay = param.Number(default=0.5)
+
+    def __init__(self, **params):
+        super().__init__(**params)
+
+        result = self._load()
+        self._time = time.time()
+
+        self._status_pane = pn.pane.Markdown(self.status, height=40, align="start", margin=(0,5,10,5))
+        self._result_pane = pn.Column(result)
+
+        button = pn.widgets.Button.from_param(self.param.run, sizing_mode="fixed")
+        self._view = pn.Column(
+            pn.Row(button, self._status_pane),
+            self._result_pane
+        )
+
+    def __panel__(self):
+        return self._view
+
+    def _start_run(self):
+        self.status = f"Running ..."
+        self._time = time.time()
+
+    def _stop_run(self):
+        now = time.time()
+        duration = round(now-self._time,3)
+        self._time = now
+        self.runs += 1
+        self.status = f"Finished run {self.runs} in {duration}sec"
+
+    @param.depends("run", watch=True)
+    def _run_with_status_update(self):
+        self._start_run()
+        self._result_pane[:] = [self._run()]
+        self._stop_run()
+
+    @param.depends("status", watch=True)
+    def _update_status_pane(self):
+        self._status_pane.object = self.status
+
+    def _load(self):
+        time.sleep(self.load_delay)
+        return "Loaded"
+
+    def _run(self):
+        time.sleep(self.run_delay)
+        return f"Result {self.runs+1}"
+
+
+if pn.state.served:
+    pn.extension(sizing_mode="stretch_width")
+
+    App().servable()
---
+++ tests/test_panel.py
@@ -0,0 +1,34 @@
+"""
+https://panel.holoviz.org/how_to/test/pytest.html
+"""
+import pytest
+
+from app import App
+
+@pytest.fixture
+def app():
+    return App(run_delay=0.001, load_delay=0.001)
+
+def test_constructor(app):
+    """Tests default values of App"""
+    # Then
+    assert app.run == False
+    assert app.status == "No runs yet"
+    assert app.runs == 0
+
+def test_run(app):
+    """Tests behaviour when Run button is clicked once"""
+    # When
+    app.param.trigger('run')
+    # Then
+    assert app.runs == 1
+    assert app.status.startswith("Finished run 1 in")
+
+def test_run_twice(app):
+    """Tests behaviour when Run button is clicked twice"""
+    # When
+    app.param.trigger('run')
+    app.param.trigger('run')
+    # Then
+    assert app.runs == 2
+    assert app.status.startswith("Finished run 2 in")
